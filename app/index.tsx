e74d57a3859f506367b40500496cdfc31c816df2
Mathieu Post <mathieupost@gmail.com>
2024-06-20 14:20:20 +0200

    setup delete latest asset example

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,34 @@
-import { Text, View } from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import { Button, View } from "react-native";
 
 export default function Index() {
+  const deleteLatestAsset = async () => {
+    try {
+      const permission = await MediaLibrary.requestPermissionsAsync();
+      if (!permission.granted) {
+        console.error("Permission not granted");
+        return;
+      }
+
+      const assets = await MediaLibrary.getAssetsAsync({
+        first: 1,
+        sortBy: ["creationTime"],
+      });
+      if (assets.assets.length === 0) {
+        console.error("No assets found");
+        return;
+      }
+
+      const asset = assets.assets[0];
+      const deleted = await MediaLibrary.deleteAssetsAsync([asset.id]);
+      if (!deleted) {
+        console.error("Failed to delete asset");
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <View
       style={{
@@ -9,7 +37,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button title="Delete latest asset" onPress={deleteLatestAsset} />
     </View>
   );
 }
